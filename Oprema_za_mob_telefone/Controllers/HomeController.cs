40e4145aa3bbbbf4a5a8bf786da22ca649c78372
Jelena Miodragovic <jelena-miodragovic@live.com>
2018-04-04 22:33:07 +0200

    dodata lista proizvoda na home page

--- Oprema_za_mob_telefone/Controllers/HomeController.cs
+++ Oprema_za_mob_telefone/Controllers/HomeController.cs
@@ -4,15 +4,38 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Oprema_za_mob_telefone.Data;
 using Oprema_za_mob_telefone.Models;
+using Oprema_za_mob_telefone.Models.HomeViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Oprema_za_mob_telefone.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public HomeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext; // Neka izmjena 1
+            // Neka izmjena 23
+        }
         public IActionResult Index()
         {
-            return View();
+            var proizvodi = this.dbContext.Proizvodi.Include(x => x.Kategorija).ToList(); //ucitava sve proizvode iz baze
+
+            var model = proizvodi.Select(proizvod => new ProizvodViewModel
+            {
+                Id = proizvod.Id,
+                Naziv = proizvod.Naziv,
+                NazivKategorije = proizvod.Kategorija.Naziv,
+                Cena = proizvod.Cena,
+                Opis = proizvod.Opis,
+                Slika = proizvod.Slika
+            }).ToArray();
+
+
+            return View(model);
         }
 
         public IActionResult About()
---
+++ Oprema_za_mob_telefone/Models/HomeViewModels/ProizvodViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oprema_za_mob_telefone.Models.HomeViewModels
+{
+    public class ProizvodViewModel
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        [Display(Name = "Kategorija")]
+        public string NazivKategorije { get; set; }
+        public decimal Cena { get; set; }
+        public string Opis { get; set; }
+        public string Slika { get; set; }
+    }
+}
